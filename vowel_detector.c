34c9894bacd93c770b624dd6fe088d9a772a1346
DurgaSaiVishnu <aiger7302@gmail.com>
2026-07-25 16:59:02 +0530

    diff-method

--- vowel_detector.c
+++ vowel_detector.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    char character;
-    printf("Enter a character: ");
-    scanf("%c", &character);
-    if ( character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ||
-         character == 'A' || character == 'E' || character == 'I' ||
-         character == 'O' || character == 'U') {
-        printf("%c is a vowel.\n", character);
-    } else {
-        printf("%c is not a vowel.\n", character);
+    char ch;
+    printf("enter a character: ");
+    scanf("%c", &ch);
+    switch(ch){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        printf("Vowel\n");
+        break;
+    default:
+    printf("consonant");    
     }
     return 0;
 }
